8197e43acaf24a5d917f3c7b65544285de16c6c4
Lara <63805048+larkgz@users.noreply.github.com>
2026-05-31 13:38:59 -0400

    fix(clangen-lite): inheritance crash

--- scripts/cat_relations/inheritance.py
+++ scripts/cat_relations/inheritance.py
@@ -330,6 +330,8 @@ class Inheritance:
         for parent_id, value in self.parents.items():
             parent_cat = self.cat.fetch_cat(parent_id)
             grandparents = self.get_parents(parent_cat)
+            if not grandparents:
+                continue
             for grand_id in grandparents:
                 if grand_id in self.parents.keys():
                     parent_relation = self.parents[grand_id]
